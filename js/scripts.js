b5ea0159815c3584c6d207b02bcaa888c9ceb896
Lee Heng Chuah <clhkk79@gmail.com>
2018-11-04 09:25:30 +0000

    staged files

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 console.log("Welcome to my portfolio")
 
 $(document).ready(function() {
-  $(".project").on("mouseenter", function() {
+  $(".project-preview").on("mouseenter", function() {
     $(this).find(".project-details").fadeIn();
     });
-  $(".project").on("mouseleave", function() {
+  $(".project-preview").on("mouseleave", function() {
     $(this).find(".project-details").fadeOut();
     });
 });
